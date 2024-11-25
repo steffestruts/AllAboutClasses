915d1b1052b8333740c426bd401d357ff21b119e
Stefan Sjöberg <bird.nerd@outlook.com>
2024-11-25 10:54:23 +0100

    Add project files.

---
+++ MainApp/Models/User.cs
@@ -0,0 +1,13 @@
+﻿namespace MainApp.Models;
+
+public class User
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public string Password { get; set; } = null!;
+
+    public string FullName => $"{FirstName} {LastName}";
+
+}
---
+++ MainApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using MainApp.Models;
+
+User user = new User();
+
+Console.WriteLine("Enter your first name: ");
+user.FirstName = Console.ReadLine()!;
+
+Console.WriteLine("Enter your last name: ");
+user.LastName = Console.ReadLine()!;
+
+
+Console.WriteLine($"Your name is {user.FullName}");
+Console.ReadKey();
