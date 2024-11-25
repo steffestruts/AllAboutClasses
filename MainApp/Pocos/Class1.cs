cfe9c2d101441688cfa6b3ae90b8cb4145f3977c
Stefan Sjöberg <bird.nerd@outlook.com>
2024-11-25 11:33:11 +0100

    Folder added

---
+++ MainApp/Pocos/Class1.cs
@@ -0,0 +1,15 @@
+﻿namespace MainApp.Pocos;
+
+public class Product
+{
+    public int Id { get; set; }
+
+    public string? ArticleNumber { get; set; }
+
+    public string Title { get; set; } = null!;
+
+    public string? Description { get; set; }
+
+    public decimal Price { get; set; }
+
+}
